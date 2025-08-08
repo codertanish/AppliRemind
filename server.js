070b071c7472770e0cc337de60fceb908e14266d
codertanishrastogi <tanishrastogi2020@gmail.com>
2025-08-08 18:22:13 -0400

    Node server and frontend ready for deployment.

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const APP_SCRIPT_URL = process.env.APP_SCRIPT_URL;
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/config', (req, res) => {
 res.json({
   scriptURL: APP_SCRIPT_URL,
-  password: ADMIN_PASSWORD
 });
 });
 
